c4618b284d168d227b290ee19f757c573feff55e
Tom <thomas.a.butler@gmail.com>
2017-12-26 10:54:14 -0700

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {render } from 'react-dom';
+
+import '../css/style.css';
+
+import keenImage from '../assets/keen.png';
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+                Hello from React
+                <img src={ keenImage } alt='Commander Keen' />
+            </div>
+        )
+    }
+}
+
+render(<Hello />, document.getElementById('app'));
